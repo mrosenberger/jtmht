bfc1577571f82db3aabf88d0eb166f534db40177
Michael Rosenberger <leahcim165@gmail.com>
2013-09-14 17:53:17 -0700

    Initial commit with rails, rubyracer, execjs

---
+++ jtmht/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jtmht::Application.config.secret_key_base = 'bd20d1f31a49e0419889723524c5fca95b8fa6a663ca23047f9836f660111270d478db567a998bdba5adb359d3322777ac97985f081ac7e2dd8c94a9e5372021'
