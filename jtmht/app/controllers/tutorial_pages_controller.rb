3a5c96dd4ab747e1569ec424ea3bb7d241279f5b
Michael Rosenberger <leahcim165@gmail.com>
2013-09-15 18:24:46 -0700

    Improved loader, added synonym hash matching and multi-version loading

--- jtmht/app/controllers/tutorial_pages_controller.rb
+++ jtmht/app/controllers/tutorial_pages_controller.rb
@@ -1,13 +1,25 @@
 class TutorialPagesController < ApplicationController
 
+	def get_verb_in_technology_for_version(technology, verb, version)
+		verb_node = TECHNOLOGIES[technology][:verbs][verb]
+		versions = verb_node[:meta][:versions]
+	end
+
   def show
-    @verb = params[:verb]
-    @technology = params[:technology]
-    @version = params[:version] || 'latest'
+    @verb = canonize_name(params[:verb])
+    @technology = canonize_name(params[:technology])
+    verb_node = TECHNOLOGIES[@technology][:verbs][@verb]
+    default_version = verb_node[:meta][:default_version]
+    @version = canonize_name(params[:version] || default_version)
+    info = TECHNOLOGIES[@technology][:verbs][@verb][:versions][@version]
   end
 
   def canonize_name(name)
-  	name.gsub(/[^0-9a-z _-]/i, '').gsub(' ', '-').gsub('_', '-').downcase
+  	if name.nil?
+  		nil
+  	else
+  		name.gsub(/[^0-9a-z _-]/i, '').gsub(' ', '-').gsub('_', '-').downcase
+  	end
   end
 
 end
--- jtmht/config/initializers/load_technologies.rb
+++ jtmht/config/initializers/load_technologies.rb
@@ -1,33 +1,60 @@
 TECHNOLOGIESPATH = Rails.root + "app/assets/technologies/"
 
 def rebuild_technologies_hash(technologies_root)
+	Rails.logger.debug "=============================================="
+	Rails.logger.debug "Rebuilding technologies hash"
 	temp = HashWithIndifferentAccess.new
 	technologies = Dir.entries(technologies_root).select { |technology| ! [".", ".."].include? technology }
+	Rails.logger.debug "Technologies found: #{technologies}"
 	technologies.each do |technology|
+		Rails.logger.debug "  Processing technology '#{technology}'"
 		# Also have to create entry for each synonym to the same technology
 		temp[technology] = HashWithIndifferentAccess.new # technologies[:java]
 		path_into_technology = technologies_root + (technology + "/")
-		Rails.logger.debug(path_into_technology + "technology.json")
 		technology_json_file_contents = File.open(path_into_technology + "technology.json").read
 		temp[technology][:meta] = ActiveSupport::JSON.decode technology_json_file_contents # Maybe wrap with hashwithindifferentaccess
 		verbs = Dir.entries(path_into_technology + "verbs/").select { |verb| ! [".", ".."].include? verb }
 		temp[technology][:verbs] = HashWithIndifferentAccess.new # technologies[:java][:verbs]
-		Rails.logger.debug(verbs)
+		technology_synonyms = temp[technology][:meta][:synonyms]
+		Rails.logger.debug "    Technology found to have the following synonyms: #{technology_synonyms}. Associating synonyms..."
+		technology_synonyms.each do |technology_synonym|
+			Rails.logger.debug "      Associating synonym '#{technology_synonym}'' with technology '#{technology}'"
+			temp[technology_synonym] = temp[technology]
+		end
+		Rails.logger.debug "    Done associating synonyms"
+		Rails.logger.debug "    Processing verbs"
 		verbs.each do |verb|
+			Rails.logger.debug "      Processing verb '#{verb}'"
 			temp[technology]['verbs'][verb] = HashWithIndifferentAccess.new # technologies[:java][:verbs][:open-file]
 			# Also have to create entry for each synonym to the same verb
 			path_into_verb = path_into_technology + ("verbs/" + verb + "/")
 			verb_json_file_contents = File.open(path_into_verb + "verb.json").read
 			temp[technology][:verbs][verb][:meta] = ActiveSupport::JSON.decode verb_json_file_contents
+			verb_synonyms = temp[technology][:verbs][verb][:meta][:synonyms]
+			Rails.logger.debug "        Verb found to have the following synonyms: #{verb_synonyms}. Associating synonyms..."
+			verb_synonyms.each do |verb_synonym|
+				Rails.logger.debug "          Associating synonym '#{verb_synonym}' with verb '#{verb}'"
+				temp[technology]['verbs'][verb_synonym] = temp[technology]['verbs'][verb]
+			end
+			Rails.logger.debug "        Done associating verb synonyms"
 			temp[technology][:verbs][verb][:versions] = HashWithIndifferentAccess.new
 			versions = Dir.entries(path_into_verb + "versions/").select { |version| ! [".", ".."].include? version }
+			
+			Rails.logger.debug "        Verb found to have the following versions: #{versions}"
+			Rails.logger.debug "        Processing versions..."
 			versions.each do |version|
+				Rails.logger.debug "            Processing version #{version}"
 				version_file_path = path_into_verb + ("versions/" + version)
 				temp[technology][:verbs][verb][:versions][version] = File.open(version_file_path).read
+				Rails.logger.debug "            Loaded tutorial with technology='#{technology}', verb='#{verb}', version='#{version}"
 			end
+			Rails.logger.debug "        Done processing versions."
+			Rails.logger.debug "      Done processing verb."
 		end
+		Rails.logger.debug "    Done processing verbs"
+		Rails.logger.debug "  Done processing technology"
 	end
-
+	Rails.logger.debug "Done processing technologies"
 	return temp
 end
 
